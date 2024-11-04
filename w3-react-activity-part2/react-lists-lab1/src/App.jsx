198d85289265ac891c8b745a11002f5d32185e7a
teemvat <teemu.vataja@metropolia.fi>
2024-11-04 10:33:53 +0200

    ready

---
+++ w3-react-activity-part2/react-lists-lab1/src/App.jsx
@@ -0,0 +1,18 @@
+import Book from './Book.jsx';
+import booksData from './booksData.jsx';
+import './Book.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Book List</h1>
+      <div className="book-list">
+        {booksData.map(book => 
+          <Book key={book.id} book={book} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
